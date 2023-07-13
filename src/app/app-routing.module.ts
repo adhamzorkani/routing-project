cf3c8f8e01b65f17418cc9926cbf57f037bd3540
adhamzorkani <adham.zorkani@aucegypt.edu>
2023-07-13 14:15:56 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingsProfileComponent } from './settings-profile/settings-profile.component';
+import { SettingsContactComponent } from './settings-contact/settings-contact.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'settings', component: SettingsComponent},
-  {path: '**', component: PageNotFoundComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent, children: [
+    {path: 'profile', component: SettingsProfileComponent},
+    {path: 'contact', component: SettingsContactComponent }
+  ]},
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
